ebb7eb4bfb871a7e90a3363ff20a3cfb32a87322
kazuooooo <matsumotokazuya7@gmail.com>
2016-03-13 12:06:53 +0900

    add graph

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'twitter-bootstrap-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'lazy_high_charts'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/heart_beats_controller.rb
+++ app/controllers/heart_beats_controller.rb
@@ -3,7 +3,24 @@ class HeartBeatsController < ApplicationController
 
   def measure
   end
+  def graph
+    time  = User.first.heart_beats.pluck(:beat_date_time).map {|time| time.strftime('%H:%M:%S')}
+    beats = User.first.heart_beats.pluck(:heart_beat)
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '心拍数')
+      f.xAxis(categories: time)
+      f.series(name: '心拍', data: beats)
+    end
 
+    # category = [1,3,5,7]
+    # current_quantity = [1000,5000,3000,8000]
+    #
+    # @graph = LazyHighCharts::HighChart.new('graph') do |f|
+    #   f.title(text: 'ItemXXXの在庫の推移')
+    #   f.xAxis(categories: category)
+    #   f.series(name: '在庫数', data: current_quantity)
+    # end
+  end
   def index
     User.first.heart_beats.create(beat_date_time: Time.now, heart_beat: params[:heart_beat])
     puts "create!!"
